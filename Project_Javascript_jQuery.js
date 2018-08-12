49518209def8f52a66747a1311b7e63c4169a32c
Nova5555 <Nova5555@gmail.com>
2018-08-12 19:08:02 -0400

    Update 1

--- Project_Javascript_jQuery.js
+++ Project_Javascript_jQuery.js
@@ -2,13 +2,12 @@
 function createGrid(x) {
     for (var rows = 0; rows < x; rows++) {
         for (var columns = 0; columns < x; columns++) {
-            $("#container").append("<div class='grid'><button class='btn'>" + rows.toString() + "-" + columns.toString() + "</button></div>");
+            //$("#container").append("<div class='grid' id='" + rows.toString().concat("," + columns.toString()) + "'>T<button class='btn' id='" + rows.toString().concat(columns.toString()) + "'>" + rows.toString() + "-" + columns.toString() + "</button></div>");
+            $("#container").append("<div class='grid' id='" + rows.toString().concat("," + columns.toString()) + "'> </div>");
         };
     };
     $(".grid").width(960/x);
     $(".grid").height(960/x);
-    $(".btn").width(600/x);
-    $(".btn").height(600/x);
 };
 
 // function that clears the grid
@@ -24,26 +23,41 @@ function refreshGrid(){
     createGrid(z);
 };
 
+function matchGridCord(gridCord){
+    return (gridCord == "5,8") ? true : false;
+}
 // create a 16x16 grid when the page loads
-// creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
 // allows the click of a button to prompt the user to create a new grid
 $(document).ready(function() {
     createGrid(16);
-
-    $(".grid").mouseover(function() {
-        //$(this).css("background-color", "black");
-        });
 
     $(".clearGrid").click(function(){
         clearGrid();
     });
 
-    $(".btn").click(function(){
-        $(this).addClass('hider');
-        //$(this).hide();
-    })
+    $('.grid').click(function(){
+        var isMatch = matchGridCord($(this).attr('id'))
+        $(this).css('background-color', 'red');
+        if (isMatch)
+        {
+            //this.text = "Found!";
+            //$(this).text("Found!"); 
+            //$(this).innerHTML = "Found!"; 
+            //$(this).html($(this).html().replace(' ','Found!'));
+            document.getElementById($(this).attr('id')).innerHTML = "Found!";
+        }
+        else
+        {
+            //this.text = "Not Found!";
+            //$(this).text("Nope!"); 
+            //$(this).innerHTML = "Not Found!"; 
+            //$(this).html($(this).html().replace(' ','Not Found!'));
+            document.getElementById($(this).attr('id')).innerHTML = "Not Found!";
+        }
+   })
 
-    $(".newGrid").click(function() {
+   $(".newGrid").click(function() {
         refreshGrid();
     });
 });
+
